e8b21b3ba44de2d53330ec68e32c12c763e756e7
Dexter Facelo <Dexter101306@gmail.com>
2026-08-11 14:34:52 +0800

    add linear search

---
+++ searching/linear/linear.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	arr := [5]int{2, 3, 5, 7, 10}
+	target := 10
+
+	for i := 0; i < len(arr); i++ {
+		if arr[i] == target {
+			fmt.Println("Found target at index:", i)
+			return
+		}
+	}
+
+	fmt.Println("Target not found")
+}
