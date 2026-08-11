9fb73c790cff6e37e5cfa5069ab92b8f34ece74b
Dexter Facelo <Dexter101306@gmail.com>
2026-08-11 14:40:31 +0800

    use range for linear search

--- searching/linear/linear.go
+++ searching/linear/linear.go
@@ -6,7 +6,7 @@ func main() {
 	arr := [5]int{2, 3, 5, 7, 10}
 	target := 10
 
-	for i := 0; i < len(arr); i++ {
+	for i := range len(arr) {
 		if arr[i] == target {
 			fmt.Println("Found target at index:", i)
 			return
